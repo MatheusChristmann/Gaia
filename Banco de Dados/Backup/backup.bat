337fbef59b0a5c9bde25fddeb0b35e7044cf83f2
Matheus Felipe Christmann <matheus.ch35@hotmail.com>
2022-11-20 09:39:19 -0300

    Conclusão das políticas de backup e restore e adição dos arquivos .bat para realizar o backup e restore do banco de dados (Teste realizado com êxito).

---
+++ Banco de Dados/Backup/backup.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set x=%DATE:~0,2%-%DATE:~3,2%-%DATE:~6,4%
+echo %x%
+set date=%x%
+echo %date%
+
+   set PGUSER=gaiadba
+   set BACKUP_FILE=C:\Backup\gaiadbBackup_%date%.backup
+   echo backup file name is %BACKUP_FILE%
+   SET PGPASSWORD=_#@gaiadba@#_
+   echo on
+   "C:\Program Files\PostgreSQL\14\bin\pg_dump.exe" -h localhost -p 5432 -U gaiadba -C -F p  -v -f %BACKUP_FILE% gaiadb
+pause
